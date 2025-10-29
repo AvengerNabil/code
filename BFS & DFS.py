7648396b5f6a9c33183442d812ed62ec4c2dfc52
S M Nabil Ausaf <smnabilausaf@gmail.com>
2025-10-29 14:43:51 +0600

    Add files via upload

---
+++ BFS & DFS.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+# Sample Graph (Tree-like structure)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+# DFS Recursive
+def dfs_recursive(node, visited=None):
+    if visited is None:
+        visited = set()
+    if node not in visited:
+        print(node, end=' ')
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs_recursive(neighbor, visited)
+
+# DFS Iterative
+def dfs_iterative(start):
+    visited = set()
+    stack = [start]
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            stack.extend(reversed(graph[node]))
+
+# BFS Iterative
+def bfs(start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=' ')
+            visited.add(node)
+            queue.extend(graph[node])
+print("DFS Recursive:")
+dfs_recursive('A')
+print("\nDFS Iterative:")
+dfs_iterative('A')
+print("\nBFS:")
+bfs('A')
