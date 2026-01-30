1bcace4e36ff30a6311c23c9e705f67b235eeddf
S M Nabil Ausaf <smnabilausaf@gmail.com>
2026-01-30 20:38:10 +0600

    codeforces

---
+++ code44.py
@@ -0,0 +1,11 @@
+n = int(input())
+pages = list(map(int, input().split()))
+
+day = 0
+while n > 0:
+    n -= pages[day]
+    if n <= 0:
+        print(day + 1)
+        break
+    day = (day + 1) % 7
+#https://codeforces.com/problemset/problem/139/A
