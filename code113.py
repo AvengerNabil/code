866a26831d3cb0b598f6d1394de4686f621a88c9
S M Nabil Ausaf <smnabilausaf@gmail.com>
2026-03-11 01:09:22 +0600

    codeforces

---
+++ code113.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    total = 0
+
+    while n > 0:
+        total += n
+        n //= 2
+
+    print(total)
+#https://codeforces.com/problemset/problem/1843/C
