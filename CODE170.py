fd3f3f2dd74ba9505458b44d8f047a59933a6418
S M Nabil Ausaf <smnabilausaf@gmail.com>
2026-07-09 02:55:40 +0600

    CODEFORCES

---
+++ CODE170.py
@@ -0,0 +1,15 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+def can_make(n, m):
+    if n == m:
+        return True
+    if n < m or n % 3 != 0:
+        return False
+    return can_make(n // 3, m) or can_make(2 * n // 3, m)
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    print("YES" if can_make(n, m) else "NO")
+#https://codeforces.com/problemset/problem/1829/D
